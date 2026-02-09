cf9a329e4413f31968e9619b6f05f86d09735232
itsdeep13 <80030947+itsdeep13@users.noreply.github.com>
2026-02-09 11:24:15 +0530

    Update vars.py

--- modules/vars.py
+++ modules/vars.py
@@ -3,18 +3,18 @@
 import os
 from os import environ
 
-API_ID = int(environ.get("API_ID", " "))
-API_HASH = environ.get("API_HASH", " ")
+API_ID = int(environ.get("API_ID", "22484497"))
+API_HASH = environ.get("API_HASH", "c38cb053916c47a97590c244663cbaef")
 BOT_TOKEN = environ.get("BOT_TOKEN", " ")
 
-OWNER = int(environ.get("OWNER", " "))
-CREDIT = environ.get("CREDIT", 'Mighty Atom')
+OWNER = int(environ.get("OWNER", "6252997817"))
+CREDIT = environ.get("CREDIT", '𓍯𝙎𝙪𝙟𝙖𝙡⚝')
 cookies_file_path = os.getenv("cookies_file_path", "youtube_cookies.txt")
 
-TOTAL_USER = os.environ.get('TOTAL_USERS', '' , ').split(',')
+TOTAL_USER = os.environ.get('TOTAL_USERS', '6252997817').split(',')
 TOTAL_USERS = [int(user_id) for user_id in TOTAL_USER]
 
-AUTH_USER = os.environ.get('AUTH_USERS', '' , ').split(',')
+AUTH_USER = os.environ.get('AUTH_USERS', '6252997817').split(',')
 AUTH_USERS = [int(user_id) for user_id in AUTH_USER]
 if int(OWNER) not in AUTH_USERS:
     AUTH_USERS.append(int(OWNER))
